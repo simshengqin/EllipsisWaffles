fb5a0840cdf4905e3ff44e623731c1e5df070101
crystalfoo1 <crystalfoo11@gmail.com>
2021-08-29 12:53:19 +0800

    Meal Subscription, View Company pages completed

--- src/app/ui/user/user-view-company/user-view-company.component.ts
+++ src/app/ui/user/user-view-company/user-view-company.component.ts
@@ -13,21 +13,9 @@ export class UserViewCompanyComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.createProduct();
     this.loadProducts();
   }
 
-  async createProduct(): Promise<void> {
-    const newProduct = {
-      name: "Chicken Rice",
-      company_id: "c1fe4026-5c99-4b2b-8bdc-6cb5eaa68d4b",
-      image_url: "",
-      type: "Meal",
-      price: 3.55,
-      quantity: 100,
-    };
-    const newProductDB = await this.api.CreateProduct(newProduct);
-  }
   loadProducts(): void {
     this.api.ListProducts().then(event => {
       this.products = event.items as Array<Product>;
